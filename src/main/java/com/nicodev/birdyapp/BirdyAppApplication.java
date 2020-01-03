7d767b3c523237cdb9b9d5b8456385e9d3ddcd93
Nico Valerga <nicodev68@gmail.com>
2020-01-03 18:07:05 -0300

    First commit

---
+++ src/main/java/com/nicodev/birdyapp/BirdyAppApplication.java
@@ -0,0 +1,13 @@
+package com.nicodev.birdyapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BirdyAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BirdyAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nicodev/birdyapp/BirdyAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nicodev.birdyapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BirdyAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
