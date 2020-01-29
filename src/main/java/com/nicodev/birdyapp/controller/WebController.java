1f5f7287aed69081b3d0cdf14b070ae2e90ea5f1
Nico Valerga <nicodev68@gmail.com>
2020-01-29 10:31:34 -0300

    Implement home html

---
+++ src/main/java/com/nicodev/birdyapp/controller/WebController.java
@@ -0,0 +1,14 @@
+package com.nicodev.birdyapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WebController {
+
+  @GetMapping("/")
+  public String showHome() {
+    return "home";
+  }
+}
