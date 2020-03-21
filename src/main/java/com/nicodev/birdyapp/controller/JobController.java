a90e9144b0ece387b6ed801b29dcf25ef37ee961
Nico Valerga <nicodev68@gmail.com>
2020-03-21 14:10:58 -0300

    Add manual contacts import

---
+++ src/main/java/com/nicodev/birdyapp/controller/JobController.java
@@ -0,0 +1,50 @@
+package com.nicodev.birdyapp.controller;
+
+import com.nicodev.birdyapp.exception.rest.BadRequestException;
+import com.nicodev.birdyapp.model.entity.User;
+import com.nicodev.birdyapp.service.ContactService;
+import com.nicodev.birdyapp.service.UserService;
+import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/jobs")
+public class JobController {
+
+  private static Logger logger = LoggerFactory.getLogger(JobController.class);
+
+  @Value("${birdy.api.key}")
+  private String birdyApiKey;
+
+  private UserService userService;
+  private ContactService contactService;
+
+  @Autowired
+  public JobController(
+      UserService userService,
+      ContactService contactService
+  ) {
+    this.userService = userService;
+    this.contactService = contactService;
+  }
+
+  @PostMapping("/import_contacts")
+  public void importContactsManually(
+      @RequestParam(value = "apikey") String apiKey
+  ) {
+    if (apiKey.isEmpty() || !apiKey.equals(birdyApiKey))
+      throw new BadRequestException("Wrong apikey!");
+
+    logger.info("Start job: import contacts manually");
+    List<User> allUsers = userService.getAllUsers();
+    allUsers.forEach(user -> contactService.updateContacts(user));
+    logger.info("End job: import contacts manually");
+  }
+}
